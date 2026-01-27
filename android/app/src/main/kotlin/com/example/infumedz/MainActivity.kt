ef1e927684cb7ae6da787f6d987a23e5dbef9a5b
S Chandra Sekhar <kingchandus143@gmail.com>
2026-01-28 00:39:38 +0530

    Video Player Done All Things V1.0

--- android/app/src/main/kotlin/com/example/infumedz/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/infumedz/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.example.infumedz
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 🔒 DISABLE SCREENSHOT & SCREEN RECORDING
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+    }
+}
